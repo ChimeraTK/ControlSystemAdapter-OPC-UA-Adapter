e1229b91cb2c69bbdc9dd516c4a1b648cd09ebff
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2023-06-23 18:50:07 +0200

    Prepare search. Add search tests.

--- tools/mapfileGenerator/ctk_opcua_generator_tools/test/testMapGenerator.py
+++ tools/mapfileGenerator/ctk_opcua_generator_tools/test/testMapGenerator.py
@@ -3,8 +3,30 @@ import logging
 from ctk_opcua_generator_tools.generatorClass import MapGenerator, XMLDirectory, XMLVar, EncryptionSettings
 
 class Test(unittest.TestCase):
+  def testFindDir(self):
+    generator = MapGenerator("test.xml")
+    ret = generator.dir.findDir("/root/Configuration")
+    self.assertEqual(ret.name, "Configuration", "Found wrong directory name")
+    ret = generator.dir.findDir("/root/NotExisting")
+    self.assertEqual(ret, None, "Found non existing directory")
+    
+  def testFindVar(self):
+    generator = MapGenerator("test.xml")
+    ret = generator.dir.findVar("/root/Configuration/test")
+    self.assertEqual(ret.name, "test", "Wrong pv name")
+    self.assertEqual(ret.fullName, "/root/Configuration/test", "Wrong full name")
+    ret = generator.dir.findVar("/root/NotExisting")
+    self.assertEqual(ret, None, "Found non existing PV")
+    
   def testReadXML(self):
     generator = MapGenerator("test.xml")
+    self.assertEqual(generator.dir.dirs[0].name, "Configuration", "Read wrong directory name")
+    self.assertEqual(generator.dir.dirs[0].newDestination, None, "Read wrong destination")
+    self.assertEqual(generator.dir.dirs[0].newDescription, None, "Read wrong description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newName, None, "Read wrong pv name")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDestination, None, "Read wrong pv destination")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDescription, None, "Read wrong pv description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newUnit, None, "Read wrong pv unit")
     if generator.certificate:
       logging.info("ok")
     generator.parseMapFile("test_mapping.xml")
@@ -21,6 +43,13 @@ class Test(unittest.TestCase):
     self.assertEqual(generator.password, "test", "Password not read from mapping file")
     self.assertEqual(generator.enableLogin, True, "Wrong login state detected.")
     self.assertEqual(generator.encryptionEnabled, True, "Encryption state detected is wrong.")
+    self.assertEqual(generator.dir.dirs[0].newDestination, "Test", "Read wrong destination")
+    self.assertEqual(generator.dir.dirs[0].newDescription, "newDescription" "Read wrong description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newName, "newName", "Read wrong pv name")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDestination, "Test", "Read wrong pv destination")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDescription, "newDescription", "Read wrong pv description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newUnit, "newUnit", "Read wrong pv unit")
+
 
     generator.parseMapFile("test_mapping_minimal.xml")
     self.assertEqual(generator.rootFolder, "Test1", "Root folder not read from mapping file")
@@ -36,7 +65,13 @@ class Test(unittest.TestCase):
     self.assertEqual(generator.password, None, "Password not read from mapping file")
     self.assertEqual(generator.enableLogin, False, "Wrong login state detected.")
     self.assertEqual(generator.encryptionEnabled, False, "Encryption state detected is wrong.")
-
+    self.assertEqual(generator.dir.dirs[0].newDestination, None, "Read wrong destination")
+    self.assertEqual(generator.dir.dirs[0].newDescription, None, "Read wrong description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newName, None, "Read wrong pv name")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDestination, None, "Read wrong pv destination")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newDescription, None, "Read wrong pv description")
+    self.assertEqual(generator.dir.dirs[0].vars[0].newUnit, None, "Read wrong pv unit")
+  
 if __name__ == "__main__":
   logging.basicConfig(format='[%(levelname)s]: %(message)s', level=logging.DEBUG)
   unittest.main()    
