911b0aa88d50d92b8dd25e601823d1b3b5485755
Zenker, Dr. Klaus (FWKE) - 126506 <k.zenker@hzdr.de>
2023-12-11 10:27:42 +0000

    Remove unused function.

--- include/ua_proxies.h
+++ include/ua_proxies.h
@@ -1,6 +1,7 @@
 /** @class ua_proxies
  * 	@brief Helper class to interact with open62541 in a easy way.
- * 	This class is a kind of a proxy to interact with the open62541 stack. For this, the class mapped all variables to the nodestore of the open62541.
+ * 	This class is a kind of a proxy to interact with the open62541 stack. For this, the class mapped all variables
+ * to the nodestore of the open62541.
  *
  * Copyright (c) 2016 Chris Iatrou <Chris_Paul.Iatrou@tu-dresden.de>
  * Copyright (c) 2016 Julian Rahm  <Julian.Rahm@tu-dresden.de>
@@ -10,11 +11,11 @@
 #ifndef HAVE_UA_PROXIES_H
 #define HAVE_UA_PROXIES_H
 
-#include <open62541/server.h>
-#include <list>
-#include <iostream>
-
 #include "ua_proxies_typeconversion.h"
+#include <open62541/server.h>
+
+#include <iostream>
+#include <list>
 
 using namespace std;
 
@@ -31,24 +32,25 @@ typedef std::list<UA_NodeId_pair*> nodePairList;
 
 /**
  * @struct UA_DataSource_Map_Element_t
- * @brief For generic callback use, this sturct contains the methode pointer and a NodeId of the model. Additinaly a description from controlSystemAdapter can be setted here.
+ * @brief For generic callback use, this sturct contains the methode pointer and a NodeId of the model. Additinaly a
+ * description from controlSystemAdapter can be setted here.
  *
  */
 typedef struct UA_DataSource_Map_Element_t {
   UA_NodeId typeTemplateId;
   UA_NodeId concreteNodeId;
-  UA_LocalizedText description;// individual description for every variable
+  UA_LocalizedText description; // individual description for every variable
   UA_DataSource dataSource;
 } UA_DataSource_Map_Element;
 typedef std::list<UA_DataSource_Map_Element> UA_DataSource_Map;
 
 #define NODE_PAIR_PUSH(_p_listname, _p_srcId, _p_targetId)                                                             \
-    do {                                                                                                               \
-        UA_NodeId_pair* tmp = new UA_NodeId_pair;                                                                      \
-        UA_NodeId_copy(&_p_srcId, &tmp->sourceNodeId);                                                                 \
-        UA_NodeId_copy(&_p_targetId, &tmp->targetNodeId);                                                              \
-        _p_listname.push_back(tmp);                                                                                    \
-    } while(0);
+  do {                                                                                                                 \
+    UA_NodeId_pair* tmp = new UA_NodeId_pair;                                                                          \
+    UA_NodeId_copy(&_p_srcId, &tmp->sourceNodeId);                                                                     \
+    UA_NodeId_copy(&_p_targetId, &tmp->targetNodeId);                                                                  \
+    _p_listname.push_back(tmp);                                                                                        \
+  } while(0);
 
 /**
  * @brief Node function and proxy mapping for new nodes
@@ -60,18 +62,5 @@ typedef std::list<UA_DataSource_Map_Element> UA_DataSource_Map;
  * @return UA_StatusCode
  */
 UA_StatusCode ua_mapInstantiatedNodes(UA_NodeId objectId, UA_NodeId definitionId, void* handle);
-
-/**
- * @brief This methode map all variables in sort of a <UA_DataSourceMap> from the called class to the open62541
- *
- * @param server	This param provides the OPC UA server
- * @param instantiatedNodesList Contains all instantiated nodes
- * @param map	Contains all Node from the class the should instantiated into the server
- * @param scrClass Pointer to our class instance
- *
- * @return UA_StatusCode
- */
-UA_StatusCode ua_callProxy_mapDataSources(
-    UA_Server* server, nodePairList instantiatedNodesList, UA_DataSource_Map* map, void* srcClass);
 
 #endif // Header include
